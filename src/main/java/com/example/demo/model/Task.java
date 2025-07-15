776eef21fe4d9843d059aa173a59aea059405513
manaya0303-admin <manaxespo@gmail.com>
2025-07-15 21:40:06 +0900

    controllerとentityのみ。

---
+++ src/main/java/com/example/demo/model/Task.java
@@ -0,0 +1,52 @@
+package com.example.demo.model;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+@Entity(name="m_task")
+@EntityListeners(AuditingEntityListener.class)
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long taskId;
+	
+	@Column(length = 100, nullable = false)
+	private String title;
+	
+	@Column(length = 500)
+	private String content;
+	
+	@Column(length = 200)
+	private String notes;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate limitDate;
+	
+	private String place;
+	
+	@CreatedDate
+	@Column(updatable = false, nullable = false)
+	private Timestamp registeredDate;
+	
+	@Basic(optional = false)
+	private boolean finishStatus;
+	
+
+}
