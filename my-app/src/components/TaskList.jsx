bbf039297a89a8392626c68e201fdc01c70d4315
manaya0303-admin <manaxespo@gmail.com>
2025-09-04 20:47:29 +0900

    完了ボタンを追加。 Chakra UIを追加。

--- my-app/src/components/TaskList.jsx
+++ my-app/src/components/TaskList.jsx
@@ -17,6 +17,15 @@ const TaskList = () => {
         fetchTasks();
     },[]);
 
+    const finishTask = async (taskId) => {
+        try {
+            await axios.put(`http://localhost:8080/todo/${taskId}/finish`)
+            setTasks(tasks.filter((task) => task.taskId !== taskId));
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
         <ul>
             {tasks
@@ -28,10 +37,10 @@ const TaskList = () => {
                         <p>メモ：{task.notes}</p>
                         <p>期限：{task.limitDate}</p>
                         <p>場所：{task.place}</p>
-                        <p>作成日：{task.registeredDate}</p>
+                        <p>作成日：{new Date(task.registeredDate).toLocaleDateString()}</p>
+                        <button onClick={() => finishTask(task.taskId)}>完了</button>
                     </li>
-                ))
-            }
+            ))}
         </ul>
     )
 }
